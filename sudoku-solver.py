84ac8fc9a4c9c8a39040e436cfd652ffed8a0efa
James Ralphs <jr.ralphs@gmail.com>
2014-11-01 22:35:47 +0100

    delete dead code

--- sudoku-solver.py
+++ sudoku-solver.py
@@ -1,13 +1,5 @@
 __author__ = 'james'
 
-from Cell import Cell
-
-a = Cell(9)
-b = Cell(None)
-
-print a.get_is_mutable()
-print b.get_is_mutable()
-
 from Board import Board
 from mocks import BoardMocks
 
