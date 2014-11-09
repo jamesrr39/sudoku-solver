b9ce368bc818f6e364290646b8ef446fd33a2c10
James Ralphs <jr.ralphs@gmail.com>
2014-11-09 20:24:14 +0100

    remove max_iterations

--- BoardSolver.py
+++ BoardSolver.py
@@ -3,16 +3,18 @@ __author__ = 'james'
 from BoardIteration import BoardIteration
 
 
-def solve(board, max_iterations):
+def solve(board):
     iteration = 0
     is_unsolved = True
-    while (iteration < max_iterations) and is_unsolved is True:
+    while is_unsolved is True:
         iteration += 1
         board_iteration = BoardIteration(board)
         board_iteration.solve()
         board = board_iteration.board
         print "squares solved: " + board_iteration.squares_solved.__str__()
         print "squares unsolved: " + board_iteration.squares_unsolved.__str__()
+        if board_iteration.squares_solved is 0:
+            return "unsolved after " + iteration.__str__() + " iterations"
         if board_iteration.squares_unsolved is 0:
             is_unsolved = False
-    return "solved in " + iteration.__str__() + " iterations" if is_unsolved is False else "unsolved after " + iteration.__str__() + " iterations"
+    return "solved in " + iteration.__str__() + " iterations"
--- sudoku-solver.py
+++ sudoku-solver.py
@@ -5,11 +5,9 @@ from mocks import BoardMocks
 import BoardSolver
 
 
-max_iterations = 100
-
 board = Board(BoardMocks.get_board_mock())
 board.print_board()
 print "\n\n"
-BoardSolver.solve(board, max_iterations)
+print BoardSolver.solve(board)
 print "\n\n"
 board.print_board()
